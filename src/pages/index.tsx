9a775dafafe5e0406ca3ec09e46ebde5f6964dd9
Jessica Barbosa <lbarbosa.jessica@gmail.com>
2023-10-17 22:36:14 -0300

    criando a estrutura da homepage

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,7 +1,12 @@
-const HomePage = () => (
-  <main className="">
-    <h1>valorant</h1>
-  </main>
+import styles from 'styles/home.module.scss'
+
+export const HomePage = () => (
+  <div className={styles.container}>
+    <header>
+      <div className={styles.header__content}>Header</div>
+    </header>
+    <main>main</main>
+  </div>
 )
 
 export default HomePage
